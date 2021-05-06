02fc495e3fb896bec97b1c68bf2ae232ee0058fc
AlanGustav0 <alangustavo946@gmail.com>
2021-05-06 08:18:45 -0300

    Terceiro Commit

--- python/repeating_string.py
+++ python/repeating_string.py
@@ -1,5 +1,4 @@
 def repeatedString(s, n):
-
     if not 'a' in s:
         return 0 
     if len(s) == 1:
@@ -9,19 +8,11 @@ def repeatedString(s, n):
     repettitions = n // len(s)
     rest = n % len(s)
     rest = s[:rest].count('a')
-    print(rest)
+
     total = repettitions * letter_quantity + rest
-
-
     print(total)
-
-
-    
-
-
 
 s = 'afcfffaged'
 n = 962645758455
-
 
 repeatedString(s, n)
